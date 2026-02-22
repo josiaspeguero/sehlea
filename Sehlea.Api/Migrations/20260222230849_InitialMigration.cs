e4db0210d7699cc163120ef9b6606c98f5747aed
josiaspegueroltjnc <josiaspegueroltjnc@gmail.com>
2026-02-22 19:11:55 -0400

    feat(api): add initial migrations

---
+++ Sehlea.Api/Migrations/20260222230849_InitialMigration.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sehlea.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Doctores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cedula = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumeroLicenciaMedica = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Especialidad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AnosExperiencia = table.Column<int>(type: "int", nullable: false),
+                    Universidad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaContratacion = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ConsultorioAsignado = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HorarioAtencion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    FechaRegistro = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doctores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pacientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cedula = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Sexo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContactoEmergencia = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TelefonoEmergencia = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaRegistro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DoctorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pacientes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pacientes_Doctores_DoctorId",
+                        column: x => x.DoctorId,
+                        principalTable: "Doctores",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Consultas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Tipo = table.Column<int>(type: "int", nullable: false),
+                    FechaConsulta = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RazonConsulta = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Observaciones = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PrecioConsulta = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PacienteId = table.Column<int>(type: "int", nullable: false),
+                    DoctorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consultas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Consultas_Doctores_DoctorId",
+                        column: x => x.DoctorId,
+                        principalTable: "Doctores",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Consultas_Pacientes_PacienteId",
+                        column: x => x.PacienteId,
+                        principalTable: "Pacientes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EstudioMedicos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Tipo = table.Column<int>(type: "int", nullable: false),
+                    Observaciones = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaEstudio = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaPubicacion = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsPublished = table.Column<bool>(type: "bit", nullable: false),
+                    PacienteId = table.Column<int>(type: "int", nullable: false),
+                    DoctorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EstudioMedicos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EstudioMedicos_Doctores_DoctorId",
+                        column: x => x.DoctorId,
+                        principalTable: "Doctores",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_EstudioMedicos_Pacientes_PacienteId",
+                        column: x => x.PacienteId,
+                        principalTable: "Pacientes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ResultadoEstudios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreDelCampo = table.Column<int>(type: "int", nullable: false),
+                    ValorEncontrado = table.Column<decimal>(type: "decimal(16,2)", nullable: false),
+                    RangoValorMinimo = table.Column<decimal>(type: "decimal(16,2)", nullable: true),
+                    RangoValorMaximo = table.Column<decimal>(type: "decimal(16,2)", nullable: true),
+                    Comentario = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EstudioId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResultadoEstudios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ResultadoEstudios_EstudioMedicos_EstudioId",
+                        column: x => x.EstudioId,
+                        principalTable: "EstudioMedicos",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consultas_DoctorId",
+                table: "Consultas",
+                column: "DoctorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consultas_PacienteId",
+                table: "Consultas",
+                column: "PacienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EstudioMedicos_DoctorId",
+                table: "EstudioMedicos",
+                column: "DoctorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EstudioMedicos_PacienteId",
+                table: "EstudioMedicos",
+                column: "PacienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pacientes_DoctorId",
+                table: "Pacientes",
+                column: "DoctorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResultadoEstudios_EstudioId",
+                table: "ResultadoEstudios",
+                column: "EstudioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Consultas");
+
+            migrationBuilder.DropTable(
+                name: "ResultadoEstudios");
+
+            migrationBuilder.DropTable(
+                name: "EstudioMedicos");
+
+            migrationBuilder.DropTable(
+                name: "Pacientes");
+
+            migrationBuilder.DropTable(
+                name: "Doctores");
+        }
+    }
+}
