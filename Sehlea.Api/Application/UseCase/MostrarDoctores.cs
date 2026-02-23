d258bc972ecc73eec79d98f7e8c3a8bffe134de1
josiaspegueroltjnc <josiaspegueroltjnc@gmail.com>
2026-02-22 21:52:31 -0400

    feat(api): add and list doctors use cases

---
+++ Sehlea.Api/Application/UseCase/MostrarDoctores.cs
@@ -0,0 +1,19 @@
+﻿using Sehlea.Api.Domain.Entities;
+using Sehlea.Api.Domain.Interfaces;
+
+namespace Sehlea.Api.Application.UseCase
+{
+    public class MostrarDoctores
+    {
+        private readonly IDoctorRepository _doctor;
+
+        public MostrarDoctores(IDoctorRepository doctor)
+        {
+            _doctor = doctor;
+        }
+        public async Task<IEnumerable<Doctor?>> MostrarDoctoresTask()
+        {
+            return await _doctor.MostrarDoctoresAsync();
+        }
+    }
+}
